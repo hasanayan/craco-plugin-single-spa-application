2c5ae23b1810988e866546c3fac0369c676cdc30
Felipe Plets <felipe@plets.me>
2022-05-25 21:14:30 -0700

    chore: fix compatibility with CRA5

--- src/override-craco-config.js
+++ src/override-craco-config.js
@@ -1,35 +1,34 @@
 const SystemJSPublicPathPlugin = require("systemjs-webpack-interop/SystemJSPublicPathWebpackPlugin");
 
 const overrideCracoConfig = ({
-    cracoConfig,
-    pluginOptions: { orgName, projectName, rootDirectoryLevel },
-  }) => {
-    if (!cracoConfig.webpack) cracoConfig.webpack = {};
-    if (!cracoConfig.webpack.plugins) cracoConfig.webpack.plugins = {};
-    if (!cracoConfig.webpack.plugins.remove)
-      cracoConfig.webpack.plugins.remove = [];
+  cracoConfig,
+  pluginOptions: { orgName, projectName, rootDirectoryLevel },
+}) => {
+  if (!cracoConfig.webpack) cracoConfig.webpack = {};
+  if (!cracoConfig.webpack.plugins) cracoConfig.webpack.plugins = {};
+  if (!cracoConfig.webpack.plugins.remove)
+    cracoConfig.webpack.plugins.remove = [];
 
-    cracoConfig.webpack.plugins.remove.push("HtmlWebpackPlugin");
-    cracoConfig.webpack.plugins.remove.push("MiniCssExtractPlugin");
+  cracoConfig.webpack.plugins.remove.push("HtmlWebpackPlugin");
+  cracoConfig.webpack.plugins.remove.push("MiniCssExtractPlugin");
 
-    cracoConfig.webpack.plugins.add = [
-      ...(cracoConfig.webpack.plugins.add || []),
-      new SystemJSPublicPathPlugin({
-        systemjsModuleName: `@${orgName}/${projectName}`,
-        rootDirectoryLevel: rootDirectoryLevel,
-      }),
-    ];
+  cracoConfig.webpack.plugins.add = [
+    ...(cracoConfig.webpack.plugins.add || []),
+    new SystemJSPublicPathPlugin({
+      systemjsModuleName: `@${orgName}/${projectName}`,
+      rootDirectoryLevel: rootDirectoryLevel,
+    }),
+  ];
 
-    cracoConfig.devServer = cracoConfig.devServer || {};
-    cracoConfig.devServer.historyApiFallback = true;
-    cracoConfig.devServer.compress = true;
-    cracoConfig.devServer.headers = {
-      ...cracoConfig.devServer.headers,
-      "Access-Control-Allow-Origin": "*",
-    };
+  cracoConfig.devServer = cracoConfig.devServer || {};
+  cracoConfig.devServer.historyApiFallback = true;
+  cracoConfig.devServer.compress = true;
+  cracoConfig.devServer.headers = {
+    ...cracoConfig.devServer.headers,
+    "Access-Control-Allow-Origin": "*",
+  };
 
-    return cracoConfig;
-  }
+  return cracoConfig;
+}
 
-  module.exports = overrideCracoConfig
-  
+module.exports = overrideCracoConfig
--- src/override-webpack-config.js
+++ src/override-webpack-config.js
@@ -4,7 +4,7 @@ const webpack = require("webpack");
 
 const getWebpackMajorVersion = () => webpack.version.split(".")[0];
 
-const buildOptimizations = (webpackConfig, minimize) => {
+const buildOptimizations = (webpackMajorVersion, webpackConfig, minimize) => {
   const optimization = {
     ...webpackConfig.optimization,
     minimize,
@@ -14,7 +14,6 @@ const buildOptimizations = (webpackConfig, minimize) => {
     }
   }
 
-  const webpackMajorVersion = getWebpackMajorVersion();
   if (webpackMajorVersion < 5) {
     optimization.namedModules = true;
     optimization.namedChunks = true;
@@ -53,63 +52,68 @@ const buildExternals = (webpackConfig, reactPackagesAsExternal, orgPackagesAsExt
 }
 
 const disableCSSExtraction = (webpackConfig) => {
-    webpackConfig.module.rules[1].oneOf.forEach((x) => {
-      if (!x.use) return;
-  
-      if (Array.isArray(x.use)) {
-        x.use.forEach((use) => {
-          if (use.loader && use.loader.includes("mini-css-extract-plugin")) {
-            use.loader = require.resolve("style-loader/dist/cjs.js");
-            delete use.options;
-          }
-        });
-      }
-    });
-  };
-  
-  const overrideWebpackConfig = ({
-    webpackConfig,
-    pluginOptions: {
-      orgName,
-      projectName,
-      entry,
-      orgPackagesAsExternal,
-      reactPackagesAsExternal,
-      minimize = false,
-      outputFilename,
-    },
-    context: { env },
-  }) => {
-    if (typeof orgName !== "string") {
-      throw Error(
-        `craco-plugin-single-spa-application requires an orgName string`
-      );
+  webpackConfig.module.rules[1].oneOf.forEach((x) => {
+    if (!x.use) return;
+
+    if (Array.isArray(x.use)) {
+      x.use.forEach((use) => {
+        if (use.loader && use.loader.includes("mini-css-extract-plugin")) {
+          use.loader = require.resolve("style-loader/dist/cjs.js");
+          delete use.options;
+        }
+      });
     }
+  });
+};
 
-    if (typeof projectName !== "string") {
-      throw Error(
-        `craco-plugin-single-spa-application requires an opts.projectName string`
-      );
-    }
-
-    webpackConfig.entry = path.resolve(entry || "src/index.js");
-    webpackConfig.output.filename = outputFilename || `${orgName}-${projectName}.js`;
-    webpackConfig.output.libraryTarget = "system";
-    webpackConfig.output.devtoolNamespace = projectName;
-    webpackConfig.output.publicPath = "";
-
-    webpackConfig.optimization = buildOptimizations(webpackConfig, minimize);
-
-    webpackConfig.module.rules.push({ parser: { system: false } });
-
-    webpackConfig.externals = buildExternals(webpackConfig, reactPackagesAsExternal, orgPackagesAsExternal);
-
-    disableCSSExtraction(webpackConfig);
-
-    systemjsInterop.checkWebpackConfig(webpackConfig);
-
-    return webpackConfig;
+const overrideWebpackConfig = ({
+  webpackConfig,
+  pluginOptions: {
+    orgName,
+    projectName,
+    entry,
+    orgPackagesAsExternal,
+    reactPackagesAsExternal,
+    minimize = false,
+    outputFilename,
+  },
+  context: { env },
+}) => {
+  if (typeof orgName !== "string") {
+    throw Error(
+      `craco-plugin-single-spa-application requires an orgName string`
+    );
   }
 
-  module.exports = overrideWebpackConfig
-  
+  if (typeof projectName !== "string") {
+    throw Error(
+      `craco-plugin-single-spa-application requires an opts.projectName string`
+    );
+  }
+
+  webpackConfig.entry = path.resolve(entry || "src/index.js");
+  webpackConfig.output.filename = outputFilename || `${orgName}-${projectName}.js`;
+  webpackConfig.output.libraryTarget = "system";
+  webpackConfig.output.devtoolNamespace = projectName;
+  webpackConfig.output.publicPath = "";
+
+  const webpackMajorVersion = getWebpackMajorVersion();
+  webpackConfig.optimization = buildOptimizations(webpackMajorVersion, webpackConfig, minimize);
+  webpackConfig.externals = buildExternals(webpackConfig, reactPackagesAsExternal, orgPackagesAsExternal);
+
+  // Reference: https://github.com/systemjs/systemjs#compatibility-with-webpack
+  if (webpackMajorVersion < 5) {
+    webpackConfig.module.rules.push({ parser: { system: false } });
+  }
+
+  disableCSSExtraction(webpackConfig);
+
+  // Reference: https://github.com/joeldenning/systemjs-webpack-interop/pull/28
+  if (webpackMajorVersion < 5) {
+    systemjsInterop.checkWebpackConfig(webpackConfig);
+  }
+
+  return webpackConfig;
+}
+
+module.exports = overrideWebpackConfig
